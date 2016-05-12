ea18bb3cc5cf9636c343fa48f7c0d1018b696668
Zak Buys <zakbuys85@gmail.com>
2016-05-12 09:45:03 +0100

    commented out new instances of mummy and daucklings in console.rb

--- console.rb
+++ console.rb
@@ -5,13 +5,13 @@ require_relative('./models/mummy_duck.rb')
 
 require( 'pry-byebug' )
 
-mummy1 = MummyDuck.new( { 'name' => 'Barbara', 'address' => 'The North Sea', 'duck_type' => 'long hauler' } ).save()
-
-baby1 = Duckling.new( { 'name' => 'Ida Jnr', 'hobby' => 'stealing gold', 'age' => 1,  'mummy_id' => mummy1.id } ).save()
-
-mummy2 = MummyDuck.new( { 'name' => 'Daphney', 'address' => 'Paddling Pool 1', 'duck_type' => 'lazybum' } ).save()
-
-baby2 = Duckling.new( { 'name' => 'Ida Jnr 2', 'hobby' => 'sleeping', 'age' => 2,  'mummy_id' => mummy1.id } ).save()
+# mummy1 = MummyDuck.new( { 'name' => 'Barbara', 'address' => 'The North Sea', 'duck_type' => 'long hauler' } ).save()
+#
+# baby1 = Duckling.new( { 'name' => 'Ida Jnr', 'hobby' => 'stealing gold', 'age' => 1,  'mummy_id' => mummy1.id } ).save()
+#
+# mummy2 = MummyDuck.new( { 'name' => 'Daphney', 'address' => 'Paddling Pool 1', 'duck_type' => 'lazybum' } ).save()
+#
+# baby2 = Duckling.new( { 'name' => 'Ida Jnr 2', 'hobby' => 'sleeping', 'age' => 2,  'mummy_id' => mummy1.id } ).save()
 
 
 
